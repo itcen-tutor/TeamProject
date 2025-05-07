03af5ea220a4a5856308d4aaf4ffb942da89d848
1TAEKIM <oneteak1@gmail.com>
2025-05-07 10:15:26 +0900

    Add wtController

---
+++ src/main/java/com/example/teamproject/controller/wtController.java
@@ -0,0 +1,17 @@
+package com.example.teamproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class wtController {
+  
+  @GetMapping("/wt")
+  public @ResponseBody String wt() {
+      return "hello, wt!!";
+  }
+  
+
+}
